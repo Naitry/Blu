6d52e14ecda4f7c9664b8dc17f67296a20b53b2e
Naitry <77989202+Naitry@users.noreply.github.com>
2024-03-31 22:18:35 -0500

    removed a print statement

--- Blu/Math/DifferentialGeometry.py
+++ Blu/Math/DifferentialGeometry.py
@@ -1,28 +1,29 @@
 import torch
 
+
 # 2d basic laplacian first order finite differences
 def laplacian(field: torch.Tensor,
-			  delta: float):
-	# Compute the Laplacian of the field
-	laplace = (-4 * field
-			   + torch.roll(field,
-							shifts=1,
-							dims=0)
-			   + torch.roll(field,
-							shifts=-1,
-							dims=0)
-			   + torch.roll(field,
-							shifts=1,
-							dims=1)
-			   + torch.roll(field,
-							shifts=-1,
-							dims=1))
-	return laplace / (delta ** 2)
+              delta: float):
+    # Compute the Laplacian of the field
+    laplace = (-4 * field
+               + torch.roll(field,
+                            shifts=1,
+                            dims=0)
+               + torch.roll(field,
+                            shifts=-1,
+                            dims=0)
+               + torch.roll(field,
+                            shifts=1,
+                            dims=1)
+               + torch.roll(field,
+                            shifts=-1,
+                            dims=1))
+    return laplace / (delta ** 2)
 
 
 def laplacianFourthOrder(field: torch.Tensor,
-						 delta: float) -> torch.Tensor:
-	"""
+                         delta: float) -> torch.Tensor:
+    """
 	Compute the Laplacian of the field using a fourth-order central difference in n-dimensions.
 	Applies Dirichlet boundary conditions (field = 0 at boundaries).
 
@@ -30,38 +31,38 @@ def laplacianFourthOrder(field: torch.Tensor,
 	:param: delta: The spacing between points in the field.
 	:return: The Laplacian of the field as an n-dimensional torch tensor.
 	"""
-	originalShape = field.shape
-	laplace = torch.zeros_like(field)
+    originalShape = field.shape
+    laplace = torch.zeros_like(field)
 
-	for dim in range(field.dim()):
-		# Apply boundary conditions for the current dimension
-		field.index_fill_(dim,
-						  torch.tensor([0, originalShape[dim] - 1],
-									   device=field.device),
-						  0)
+    for dim in range(field.dim()):
+        # Apply boundary conditions for the current dimension
+        field.index_fill_(dim,
+                          torch.tensor([0, originalShape[dim] - 1],
+                                       device=field.device),
+                          0)
 
-		# Fourth-order central difference for the current dimension
-		laplace.add_(-1 * torch.roll(field,
-									 shifts=2,
-									 dims=dim)
-					 - 1 * torch.roll(field,
-									  shifts=-2,
-									  dims=dim)
-					 + 16 * torch.roll(field,
-									   shifts=1,
-									   dims=dim)
-					 + 16 * torch.roll(field,
-									   shifts=-1,
-									   dims=dim)
-					 - 30 * field)
+        # Fourth-order central difference for the current dimension
+        laplace.add_(-1 * torch.roll(field,
+                                     shifts=2,
+                                     dims=dim)
+                     - 1 * torch.roll(field,
+                                      shifts=-2,
+                                      dims=dim)
+                     + 16 * torch.roll(field,
+                                       shifts=1,
+                                       dims=dim)
+                     + 16 * torch.roll(field,
+                                       shifts=-1,
+                                       dims=dim)
+                     - 30 * field)
 
-	laplace.divide_(12 * delta ** 2)
+    laplace.divide_(12 * delta ** 2)
 
-	# Reinforce boundary conditions after computing the Laplacian
-	for dim in range(field.dim()):
-		laplace.index_fill_(dim,
-							torch.tensor([0, originalShape[dim] - 1],
-										 device=field.device),
-							0)
+    # Reinforce boundary conditions after computing the Laplacian
+    for dim in range(field.dim()):
+        laplace.index_fill_(dim,
+                            torch.tensor([0, originalShape[dim] - 1],
+                                         device=field.device),
+                            0)
 
-	return laplace
+    return laplace
