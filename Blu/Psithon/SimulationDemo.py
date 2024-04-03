a540bf195f0f50c879f8b58f5022ffbede4f9987
Naitry <77989202+Naitry@users.noreply.github.com>
2024-04-02 23:55:14 -0500

    New Simulation Settings

--- Blu/Psithon/SimulationDemo.py
+++ Blu/Psithon/SimulationDemo.py
@@ -4,14 +4,18 @@ from Blu.Psithon.Universe import Universe
 
 def main():
     U: Universe = Universe(spatialDimensions=2,
-                           resolution=2000,
+                           resolution=1000,
                            dt=1e-6,
                            delta=1e-1,
-                           simulationFolderPath="./simulations/")
+                           simulationFolderPath="/mnt/nfs/simulations/")
 
     U.addField(name="2D_boson_field")
     U.fields[0].addWavePacket(packetSize=500,
-                              k=[0.5, 0.5])
+                              k=[-0.5, -0.5],
+                              position=[750, 750])
+    U.fields[0].addWavePacket(packetSize=500,
+                              k=[0.5, 0.5],
+                              position=[250, 250])
     U.fields[0].printField(clear=False)
     U.runSimulation(numSteps=int(3e7))
 
