2d12c0042a36bb87d6c76badd52725ff3d79f82c
naitry <tsteffen@naeno.com>
2024-04-05 21:01:20 -0500

    adding sim loading

--- Blu/Psithon/Simulation.py
+++ Blu/Psithon/Simulation.py
@@ -1,5 +1,6 @@
+from __future__ import annotations
 import torch
-from Blu.Psithon.Field import Field
+from Blu.Psithon.Field import Field, loadFieldComponentDict
 
 
 class Simulation():
@@ -14,7 +15,33 @@ class Simulation():
     def addFieldTimestep(self,
                          field: Field):
         if self.fields[field.name] is None:
-            self.fields[field.name] = [Field]
+            self.fields[field.name] = [Field.Tensor]
         else:
             self.fields[field.name].append(Field.Tensor)
         pass
+
+    def addTensorTimestep(self,
+                          fieldName: str,
+                          tensor: torch.Tensor):
+        if self.fields[fieldName] is None:
+            self.fields[fieldName] = [tensor]
+        else:
+            self.fields[fieldName].append(tensor)
+        pass
+
+    def loadSimField(simDir: str,
+                     fieldName: str) -> Simulation:
+        realData: dict[int, torch.Tensor] = loadFieldComponentDict(filepath=fieldName,
+                                                                   prefix="real")
+        imaginaryData: dict[int, torch.Tensor] = loadFieldComponentDict(filepath=fieldName,
+                                                                        prefix="imaginary")
+        S: Simulation = Simulation()
+
+        assert len(realData) == len(imaginaryData), "Real and Imaginary datasets do not have the same length"
+
+        for timestep, realComponent in realData:
+            imaginaryComponent = imaginaryData[timestep]
+
+            fieldTensor: torch.Tensor = torch.complex(real=realComponent,
+                                                      imag=imaginaryComponent)
+            S.addTensorTimestep()
